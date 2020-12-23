dd518585f18ecb56a68a8a101e2c0407347d3b4a
Mini-Dudu <xia0dudu@foxmail.com>
2020-12-23 19:07:51 +0800

    修改部分bug,测试注册与登入成功

--- models/user.go
+++ models/user.go
@@ -2,25 +2,29 @@ package models
 
 import (
 	"bitcoin-Core/db"
-	"bitcoin-Core/utils"
 	"fmt"
 )
 
 type User struct {
 	UserName string `form:"username"`
 	Password string `form:"password"`
+	MailBox string `form:"email"`//邮箱
 }
 
 //用于保存用户信息
 func (u User) SaveUser() error {
 	fmt.Println("注册",u.UserName)
 	fmt.Println("注册",u.Password)
-	return db.SaveUser(utils.PATH, u.UserName, u.Password)
+	err := db.SaveUserInfo(u.UserName, u.MailBox, u.Password)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 //用于查询用户信息
 func (u User) QueryUser() (bool,error) {
 	fmt.Println("查询",u.Password)
 	fmt.Println("查询",u.UserName)
-	return db.Query(u.UserName,u.Password)
+	return db.QueryByName(u.UserName,u.Password)
 }
 
