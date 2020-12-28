3c4dc02f77109051b197cd382c510e0ece572e0d
Mini-Dudu <xia0dudu@foxmail.com>
2020-12-28 20:24:16 +0800

    测试登入与注册逻辑,无Bug

--- controllers/Login.go
+++ controllers/Login.go
@@ -23,35 +23,35 @@ func (l *LoginController) Post() {
 		return
 	}
 
-	if user.UserName!= "" {
+	if user.UserName != "" {
 		b, err := user.QueryUserByUserName()
 		if err != nil {
 			fmt.Println(err.Error())
 			l.Ctx.WriteString("用户信息查询失败，请检查账户信息！")
 			return
 		}
-		if b == false {
+		if !b {
 			l.Ctx.WriteString("用户或密码错误请重试！")
 			return
 		}
-		if b == true {
+		if b {
 			l.TplName = "home.html"
 
 		}
 
 	}
-	if user.MailBox!="" {
+	if user.MailBox != "" {
 		b, err := user.QueryUserByEmail()
 		if err != nil {
 			fmt.Println(err.Error())
 			l.Ctx.WriteString("用户信息查询失败，请检查账户信息！")
 			return
 		}
-		if b == false {
+		if !b {
 			l.Ctx.WriteString("邮箱或密码错误请重试！")
 			return
 		}
-		if b == true {
+		if b {
 			l.TplName = "home.html"
 
 		}
